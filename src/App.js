d169c32032f47bb6c8180685f9c70bf91376de81
gilbertosantana24 <gilbertobuja@gmail.com>
2022-07-10 13:58:39 -0700

    connect to api

--- src/App.js
+++ src/App.js
@@ -4,22 +4,48 @@ import { useState } from 'react';
 function App() {
 
   const myArr = [
-    [-1, 5, -1, 9, -1, -1, -1, -1, -1],
-    [8, -1, -1, -1, 4, -1, 3, -1, 7],
-    [-1, -1, -1, 2, 8, -1, 1, 9, -1],
-    [5, 3, 8, 6, -1, 7, 9, 4, -1],
-    [-1, 2, -1, 3, -1, 1, -1, -1, -1],
-    [1, -1, 9, 8, -1, 4, 6, 2, 3],
-    [9, -1, 7, 4, -1, -1, -1, -1, -1],
-    [-1, 4, 5, -1, -1, -1, 2, -1, 9],
-    [-1, -1, -1, -1, 3, -1, -1, 7, -1]
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
   ]
 
     const [sudokuArr, setSudokuArr] = useState(myArr);
 
-    function onInputChange() {
-
+    function onInputChange(e, row, col) {
+      setSudokuArr(() => {
+        const updatedArr = sudokuArr
+        for(let i = 0; i < updatedArr.length; i++){
+          for(let j = 0; j < updatedArr[i].length; j++){
+            if(i == row && j == col){
+                updatedArr[i][j] = e.target.value
+            }
+          }
+        }
+        return updatedArr
+      })
     }
+
+  function solveClick () {
+    (async () => {
+      const api = await fetch('https://sudoku-solver-jesushzv.herokuapp.com/', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({board : sudokuArr})
+      });
+      const content = await api.json();
+      setSudokuArr(content)
+    })();
+  }
+
 
   return (
     <div className="App">
@@ -32,7 +58,7 @@ function App() {
                 return <tr key={rIndex} className={(row + 1) % 3 === 0 ? 'bottomBorder' : ''}>
                   {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((col, cIndex) => {
                     return <td key={rIndex + cIndex} className={(col + 1) % 3 === 0 ? 'sideBorder' : ''}>
-                    <input onChange={(e) => onInputChange(e, row, col)} value={sudokuArr[row][col] === -1 ? '' : sudokuArr[row][col]} className='cell-block' disabled={myArr[row][col] !== -1} />
+                    <input onChange={(e) => onInputChange(e, row, col)} className='cell-block' disabled={myArr[row][col] !== -1} />
                   </td>
                   })}
                 </tr>
@@ -41,7 +67,7 @@ function App() {
           </tbody>
         </table>
         <div className='buttons'>
-          <button className='solveBtn'>Solve</button>
+          <button className='solveBtn' onClick={() => solveClick()}>Solve</button>
           <button className='resetBtn'>Reset</button>
         </div>
       </div>
